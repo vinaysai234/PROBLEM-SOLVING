fb2c31b39f62e62ce7bd394c02bc663c13e062ba
vinay sai <vinayofficial2006@gmail.com>
2026-06-17 08:58:26 +0530

    Add palindrome check functionality in C++

---
+++ Hackerarth/palindromecheck.cpp
@@ -0,0 +1,22 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(const string &s) {
+    int i = 0, j = (int)s.size() - 1;
+    while (i < j) {
+        while (i < j && !isalnum((unsigned char)s[i])) ++i;
+        while (i < j && !isalnum((unsigned char)s[j])) --j;
+        if (tolower((unsigned char)s[i]) != tolower((unsigned char)s[j])) return false;
+        ++i; --j;
+    }
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    if (!getline(cin, s)) return 0;
+    cout << (isPalindrome(s) ? "true" : "false") << "\n";
+    return 0;
+}
