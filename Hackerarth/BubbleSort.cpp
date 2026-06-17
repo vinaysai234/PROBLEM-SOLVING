b515433a430e4b0e70eb5fdcf76917afb65016f1
vinay sai <vinayofficial2006@gmail.com>
2026-06-17 08:55:59 +0530

    Create BubbleSort.cpp

---
+++ Hackerarth/BubbleSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    int swaps = 0;
+    
+    // Bubble sort with swap counting
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swaps++;
+            }
+        }
+    }
+    
+    cout << swaps << "\n";
+    
+    return 0;
+}
